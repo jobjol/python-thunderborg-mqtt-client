3380415f52994de587eb1d2417067e23bb9dea3a
Job Joling <job@jobjol.com>
2020-06-20 15:03:59 +0200

    Add two stepper motors logic

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
 client_name = 'raspberry_1'
 broker = 'home.jobjol.com'
 port = 1883
+
+# Tell the system how to drive the stepper
+maxPower = 1.00                         # Output to drive the stepper
+holdingPower = 0.50                     # Output to drive the stepper when holding
+sequence = [                            # Order for stepping
+    [+maxPower, +maxPower],
+    [+maxPower, -maxPower],
+    [-maxPower, -maxPower],
+    [-maxPower, +maxPower]]
+sequenceHold = [                        # Order for stepping at holding power
+    [+holdingPower, +holdingPower],
+    [+holdingPower, -holdingPower],
+    [-holdingPower, -holdingPower],
+    [-holdingPower, +holdingPower]]
+stepDelay = 0.002
--- mqttStepper.py
+++ mqttStepper.py
@@ -3,6 +3,8 @@ import time
 import os
 import subprocess
 import config
+import ThunderBorg
+import sys
 # Create a credentials.py file with the following structure:
 #
 # login = dict(
@@ -14,6 +16,28 @@ try:
 except ImportError:
     credentials = None
 
+# Name the global variables
+global step
+global TB
+
+# Setup the ThunderBorg
+TB = ThunderBorg.ThunderBorg()     # Create a new ThunderBorg object
+#TB.i2cAddress = 0x15              # Uncomment and change the value if you have changed the board address
+TB.Init()                          # Set the board up (checks the board is connected)
+if not TB.foundChip:
+    boards = ThunderBorg.ScanForThunderBorg()
+    if len(boards) == 0:
+        print 'No ThunderBorg found, check you are attached :)'
+    else:
+        print 'No ThunderBorg at address %02X, but we did find boards:' % (TB.i2cAddress)
+        for board in boards:
+            print '    %02X (%d)' % (board, board)
+        print 'If you need to change the I²C address change the setup line so it is correct, e.g.'
+        print 'TB.i2cAddress = 0x%02X' % (boards[0])
+    sys.exit()
+step = -1
+
+
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to broker")
@@ -22,15 +46,68 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Connection failed")
 
+
 def on_message(client, userdata, message):
-    if message.payload == 'ON':
-        print "Message received: " + message.topic + " : " + message.payload
-        os.system('clear')
-        time.sleep(1.5)
-        if message.topic == 'Misc/PrayerTrigger':
-            subprocess.Popen(["omxplayer", "/home/pi/share/adhan.mp4", "--aspect-mode", "fill"])
+    if message.topic == 'home/living/curtain/left':
+        if is_number(message.payload):
+            moveStep(steps, 0)
+
+    if message.topic == 'home/living/curtain/right':
+        if is_number(message.payload):
+            moveStep(steps, 1)
+
+# Function to perform a sequence of steps as fast as allowed
+def moveStep(count, motornumber):
+    global step
+    global TB
+
+    # Choose direction based on sign (+/-)
+    if count < 0:
+        dir = -1
+        count *= -1
+    else:
+        dir = 1
+
+    # Loop through the steps
+    while count > 0:
+        # Set a starting position if this is the first move
+        if step == -1:
+            drive = config.sequence[-1]
+            if motornumber == 0:
+                TB.SetMotor1(drive[motornumber])
+            if motornumber == 1:
+                TB.SetMotor2(drive[motornumber])
+            step = 0
         else:
-            subprocess.Popen(["omxplayer", "/home/pi/share/adhanfajr.mp4", "--aspect-mode", "fill"])
+            step += dir
+
+        # Wrap step when we reach the end of the sequence
+        if step < 0:
+            step = len(config.sequence) - 1
+        elif step >= len(config.sequence):
+            step = 0
+
+        # For this step set the required drive values
+        if step < len(config.sequence):
+            drive = config.sequence[step]
+            if motornumber == 0:
+                TB.SetMotor1(drive[motornumber])
+            if motornumber == 1:
+                TB.SetMotor2(drive[motornumber])
+        time.sleep(config.stepDelay)
+        count -= 1
+
+# Function to switch to holding power
+def HoldPosition(motornumber):
+    global step
+    global TB
+
+    # For the current step set the required holding drive values
+    if step < len(config.sequence):
+        drive = config.sequenceHold[step]
+        TB.SetMotor1(drive[0])
+        TB.SetMotor2(drive[1])
+
 
 Connected = False
 
@@ -49,8 +126,8 @@ client.loop_start()
 while Connected != True:
     time.sleep(0.1)
 
-client.subscribe('Misc/PrayerTrigger')
-client.subscribe('Misc/PrayerTriggerFajr')
+client.subscribe('home/living/curtain/left')
+client.subscribe('Mhome/living/curtain/right')
 
 try:
     while True:
