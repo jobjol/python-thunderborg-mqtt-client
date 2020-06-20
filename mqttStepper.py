aa0b826c236d554c27978270d2a7714ff2ab02d4
Job Joling <job@jobjol.com>
2020-06-20 13:49:01 +0200

    Coding standards

--- mqttStepper.py
+++ mqttStepper.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-\
+
+import context
 import paho.mqtt.client as mqtt
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -8,15 +13,17 @@ def on_connect(client, userdata, flags, rc):
     # reconnect then subscriptions will be renewed.
     client.subscribe("$SYS/#")
 
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.178.106", 1883, 60)
+client.connect("home.jobjol.com", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
