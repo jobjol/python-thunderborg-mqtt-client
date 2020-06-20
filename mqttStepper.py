fb2ff85eb4a0e154f0f479b97304849a02aa6158
Job Joling <job@jobjol.com>
2020-06-20 15:23:45 +0200

    testing

--- mqttStepper.py
+++ mqttStepper.py
@@ -42,7 +42,7 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, message):
-    print (message.topic)
+    print ('message incoming')
     print (message.payload)
     if message.topic == 'home/living/curtain/left':
         if is_number(message.payload):
