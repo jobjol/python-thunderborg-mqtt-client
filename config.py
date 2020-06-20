ab8dd6829b89d3e463355e8fa19b53bcf6bf02d3
Job Joling <job@jobjol.com>
2020-06-20 14:11:43 +0200

    Implement config file.

---
+++ config.py
@@ -0,0 +1,7 @@
+login = dict(
+    username = 'xxx',
+    password = 'xxx',
+)
+client_name = 'raspberry_1'
+broker = 'hostname or ipaddress'
+port = 1883
--- mqttStepper.py
+++ mqttStepper.py
@@ -1,32 +1,52 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-\
+import paho.mqtt.client as mqttClient
+import time
+import os
+import subprocess
+import config
 
-import context
-import paho.mqtt.client as mqtt
-
-
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    if rc == 0:
+        print("Connected to broker")
+        global Connected
+        Connected =True
+    else:
+        print("Connection failed")
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+def on_message(client, userdata, message):
+    if message.payload == 'ON':
+        print "Message received: " + message.topic + " : " + message.payload
+        os.system('clear')
+        time.sleep(1.5)
+        if message.topic == 'Misc/PrayerTrigger':
+            subprocess.Popen(["omxplayer", "/home/pi/share/adhan.mp4", "--aspect-mode", "fill"])
+        else:
+            subprocess.Popen(["omxplayer", "/home/pi/share/adhanfajr.mp4", "--aspect-mode", "fill"])
 
+Connected = False
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
-
-client = mqtt.Client()
+broker_address = config.broker #Your MQTT broker IP address
+port = config.port #default port change as required
+user = config.login['username'] #mqtt user name change as required
+password = config.login['password'] #mqtt password change as required
+client = mqttClient.Client(config.client_name)
+client.username_pw_set(user, password=password)
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("home.jobjol.com", 1883, 60)
+client.connect(broker_address, port=port)
+client.loop_start()
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+while Connected != True:
+    time.sleep(0.1)
+
+client.subscribe('Misc/PrayerTrigger')
+client.subscribe('Misc/PrayerTriggerFajr')
+
+try:
+    while True:
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print "exiting"
+    client.disconnect()
+    client.loop_stop()
